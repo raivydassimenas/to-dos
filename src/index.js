a0175942d8150add2b931554500fbce2b6b7a930
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2023-01-24 19:11:29 +0200

    Refactor project list and todos

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import todo from "./todo";
 import project from "./project";
 import { format } from 'date-fns';
 
-let projectList = localStorage.getItem('projectList');
+let projectList = JSON.parse(localStorage.getItem('projectList'));
 
 const content = document.querySelector("#content");
 
@@ -11,47 +11,57 @@ const menu = document.createElement("ul");
 const tab = document.createElement("div");
 tab.classList.add("tab");
 
-const projects = document.createElement("li");
-projects.addEventListener("click", (e) => {
-    tab.innerHTML = "";
+const createProjectList = function () {
+    const projectListElem = document.createElement('ul');
 
     projectList.forEach(elem => {
-        const project = document.createElement("div");
+        const project = document.createElement("li");
 
-        const title = document.createElement("h2");
-        title.classList.add("project-title");
-        title.innerText = elem.title;
+        project.innerText = elem.title;
         project.appendChild(title);
 
-        const todoList = document.createElement("ul");
-        todoList.classList.add("todo-list");
-
-        elem.todos.forEach(todoElem => {
-            const todoLi = document.createElement("li");
-            todoLi.classList.add("todo-li");
-
-            const todoLiTitle = document.createElement("div");
-            todoLiTitle = todoElem.title;
-            todoLi.appendChild(todoLiTitle);
-
-            const todoLiDescription = document.createElement("div");
-            todoLiDescription = todoElem.description;
-            todoLi.appendChild(todoLiDescription);
-
-            const todoLiDueDate = document.createElement("div");
-            todoLiDueDate = format(todoElem.dueDate, "MMM d, yyyy");
-            todoLi.appendChild(todoLiDueDate);
-
-            const todoLiPriority = document.createElement("div");
-            todoLiPriority = todoElem.priority;
-            todoLi.appendChild(todoLiPriority);
-
-            todoList.appendChild(todoLi);
-        });
-
-        project.appendChild(todoList);
+        projectListElem.appendChild(project);
     });
-});
+    
+    return projectListElem;
+};
+
+const createProjectTodoList = function(project) {
+    const projectTodoListElem = document.createElement('ul');
+
+    project.todos.forEach(elem => {
+        const todo = document.createElement('li');
+
+        todo.innerText = elem.title;
+        projectTodoListElem.appendChild(todo);
+    });
+
+    return projectTodoListElem;
+};
+
+const todoDetails = function (todo) {
+
+    const todoElem = document.createElement("div");
+    todoElem.classList.add("todo-elem");
+
+    const todoTitle = document.createElement("div");
+    todoTitle = todo.title;
+    todoElem.appendChild(todoTitle);
+
+    const todoDescription = document.createElement("div");
+    todoDescription = todo.description;
+    todo.appendChild(todoDescription);
+
+    const todoDueDate = document.createElement("div");
+    todoDueDate = format(todo.dueDate, "MMM d, yyyy");
+    todo.appendChild(todoDueDate);
+
+    const todoPriority = document.createElement("div");
+    todoPriority = todoElem.priority;
+    todo.appendChild(todoPriority);
+
+    return todo;
+}
 
 const newProjectDiv = document.createElement("div");
 newProjectDiv.classList.add("new-project-div");
